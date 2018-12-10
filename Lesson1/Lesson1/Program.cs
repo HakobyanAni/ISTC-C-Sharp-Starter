5d2b3875ec846e1cdd5b392ccab87caf24bff09c
ISTC CREATE LAB PC <ISTC CREATE LAB PC@ISTC-CREATE-LAB>
2018-12-10 20:55:12 +0400

    object, class, instance

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Lesson1
 {
+    class Tree
+    {
+        public int Height { get; set; }
+
+        public void Oror(bool qami)
+        {
+            if(qami)
+                Console.WriteLine("es ororvum em");
+            else
+            {
+                Console.WriteLine("es chem ororvum");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Tree car1 = new Tree();
+            car1.Height = 10;
+
+            Tree car2 = car1;
+
+            car2.Height = 20;
+
+            //car1.Oror(true);
+            //car2.Oror(false);
+
+            Console.WriteLine(car1.Height);
+            Console.WriteLine(car2.Height);
+
+
+            Console.ReadKey();
+
         }
     }
 }

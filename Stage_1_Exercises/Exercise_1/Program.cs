1fca54f3d67f12663005551e2f84f51669f8964f
annie .h.h <annie2016l@gmail.com>
2018-12-12 01:19:31 +0400

    Exercise

--- Stage_1_Exercises/Exercise_1/Program.cs
+++ Stage_1_Exercises/Exercise_1/Program.cs
@@ -10,14 +10,17 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
+            // Խնդիր_1:
+            //Ներմուծել քառանիշ թիվ և հաշվել նրա թվանշանների գումարը:
+
             string s = Console.ReadLine();
             int number = Convert.ToInt32(s);
-            int first_digit = number / 1000;
-            int second_digit = number % 1000 / 100;
-            int third_digit = number % 100 / 10;
-            int fourth_digit = number % 10;
-            int sum_of_digit = first_digit + second_digit + third_digit + fourth_digit;
-            Console.WriteLine(sum_of_digit);
+            int firstdigit = number / 1000;
+            int seconddigit = number % 1000 / 100;
+            int thirddigit = number % 100 / 10;
+            int fourthdigit = number % 10;
+            int sumofdigit = firstdigit + seconddigit + thirddigit + fourthdigit;
+            Console.WriteLine(sumofdigit);
             Console.ReadKey();
         }
     }

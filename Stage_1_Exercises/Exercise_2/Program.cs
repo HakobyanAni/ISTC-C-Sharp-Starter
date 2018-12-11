6c426638de495dc8b16e14a4fb5d175e52e8a489
annie .h.h <annie2016l@gmail.com>
2018-12-11 23:20:21 +0400

    Խնդիր_2: Տրված են K և N (N > 0) ամբողջ թվերը։ N անգամ արտածել K թիվը։

---
+++ Stage_1_Exercises/Exercise_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int K = Int32.Parse(s1);
+            string s2 = Console.ReadLine();
+            int N = Int32.Parse(s2);
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(K + "\0");
+            }
+            Console.ReadKey();
+        }
+    }
+}

6f4108a2f2245d396d36f3cf5c48b6b28d4863d2
annie .h.h <annie2016l@gmail.com>
2018-12-11 23:45:33 +0400

    Խնդիր_4: Ներածել n բնական թիվը։ Արտածել n–ի 1–ից մինչև 9 աստիճանների արժեքները։

---
+++ Stage_1_Exercises/Exercise_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s4 = Console.ReadLine();
+            int Num = Convert.ToInt32(s4);
+            Console.WriteLine("{0} pow 1 = {1}", Num, Num);
+            for (int i = 2; i < 10; i++)
+            {
+                int N_num = Num;
+                for (int j = 1; j < i; j++)
+                {
+                    N_num *= Num;
+                }
+                Console.WriteLine("{0} pow {1} = {2}", Num, i, N_num);
+            }
+            Console.ReadKey();
+        }
+    }
+}

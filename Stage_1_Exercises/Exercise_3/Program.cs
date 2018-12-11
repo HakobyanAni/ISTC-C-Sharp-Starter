db905d600183d1b4d17e2a2558c983381d03d26c
annie .h.h <annie2016l@gmail.com>
2018-12-11 23:36:07 +0400

    Խնդիր_3: Ներածել n միանիշ թիվը։ Արտածել n-ին չգերազանցող թվանշանները։

--- Stage_1_Exercises/Exercise_3/Program.cs
+++ Stage_1_Exercises/Exercise_3/Program.cs
@@ -10,6 +10,14 @@ namespace Exercise_3
     {
         static void Main(string[] args)
         {
+            string s3 = Console.ReadLine();
+            int n = Convert.ToInt32(s3);
+            while (n >= 0)
+            {
+                Console.Write(n + "\0");
+                n--;
+            }
+            Console.ReadKey();
         }
     }
 }

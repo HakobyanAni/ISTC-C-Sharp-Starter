cd75b229c49a15f48c615bc2a4e642a13ec591e1
annie .h.h <annie2016l@gmail.com>
2018-12-13 19:22:28 +0400

    Խնդիր_3: Ներածել n միանիշ թիվը։ Արտածել n-ին չգերազանցող թվանշանները։

---
+++ Stage_1_Exercises/Exercise_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Խնդիր_3:
+            // Ներածել n միանիշ թիվը։ Արտածել n-ին չգերազանցող թվանշանները։
+            string s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            while (n >= 0)
+            {
+                Console.Write(n + "\0");
+                n--;
+            }
+            Console.ReadKey();
+        }
+    }
+}

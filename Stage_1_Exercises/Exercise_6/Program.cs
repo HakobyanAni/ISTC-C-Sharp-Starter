35f1b358c38af8f0a8abda40af214c4197642776
annie .h.h <annie2016l@gmail.com>
2018-12-13 19:36:26 +0400

    Exercise

---
+++ Stage_1_Exercises/Exercise_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Խնդիր_6:
+            //Տրված են A և B(A < B) ամբողջ թվերը։ Արտածել աճման կարգով[A, B] հատվածին պատկանող 
+            //ամբողջ թվերը, ապա նաև նրանց քանակը։
+
+            string numb1 = Console.ReadLine();
+            int A = Int32.Parse(numb1);
+            string numb2 = Console.ReadLine();
+            int B = Int32.Parse(numb2);
+            int counter = 0;
+            while (A <= B)
+            {
+                Console.Write(A + "\0");
+                ++counter;
+                A++;
+            }
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}

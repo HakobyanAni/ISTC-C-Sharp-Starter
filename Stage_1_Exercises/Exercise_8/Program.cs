b519c6dadacfa3c5bd044eed59f7bf3c84d11ad4
annie .h.h <annie2016l@gmail.com>
2018-12-22 09:59:26 +0400

    Exercise

---
+++ Stage_1_Exercises/Exercise_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Խնդիր_8:
+            // Տրված են A և B(A < B) ամբողջ թվերը։ Գտնել[A, B] հատվածին պատկանող 3-ին պատիկ առաջին թիվը։
+
+            string stringA = Console.ReadLine();
+            int A = Convert.ToInt32(stringA);
+            string stringB = Console.ReadLine();
+            int B = Convert.ToInt32(stringB);
+            int div = 0;
+            while (A <= B)
+            {
+                if (A % 3 == 0)
+                {
+                    div = A;
+                    break;
+                }
+                A++;
+            }
+            Console.Write(div);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stage_1_Exercises/Exercise_9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Խնդիր_9:
+            // Տրված է N բնական թիվը։ Հաշվել 1 + 1 / 2 + 1 / 3 + … +1 / N գումարը(այն իրական թիվ է)։
+
+            string s = Console.ReadLine();
+            double N = Convert.ToDouble(s);
+            double N_sm = 1.0;
+            for (int i = 2; i <= N; i++)
+            {
+                N_sm += ((double)(1) / i);
+            }
+            Console.WriteLine(N_sm);
+            Console.ReadKey();
+        }
+    }
+}

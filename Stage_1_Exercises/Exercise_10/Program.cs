4d4a2aa845937b506a3a0f345000dd5c400dfd84
annie .h.h <annie2016l@gmail.com>
2018-12-23 18:06:19 +0400

    Exercise

---
+++ Stage_1_Exercises/Exercise_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Խնդիր_10:
+            // Տրված է N բնական թիվը։ Հաշվել N2 +(N + 1)2 + (N + 2)2 + … +(2·N)2 գումարը։
+            string @string = Console.ReadLine();
+            int number = Convert.ToInt32(@string);
+            int progres = 3 * number * (number + 1);
+            Console.WriteLine(progres);
+            Console.ReadKey();
+        }
+    }
+}

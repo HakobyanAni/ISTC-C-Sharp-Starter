8c2feca1fbf6dbfd9c4af904b82f1252c24fc93b
annie .h.h <annie2016l@gmail.com>
2018-12-28 21:44:51 +0400

    Exercise

---
+++ Stage_1_Exercises/Exercise_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Խնդիր_12:
+            // Ներածել n թիվը։ Արտածել yes, եթե այն հավասար է իր թվանշանների գումարի կրկնապատիկին, no՝ հակառակ դեպքում։
+
+            string s = Console.ReadLine();
+            int number = Convert.ToInt32(s);
+            int n = number;
+            int sumOfDigits = 0;
+            while (number != 0)
+            {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine((sumOfDigits* 2 == n) ? "yes" : "no");
+            Console.ReadKey();
+        }
+    }
+}
